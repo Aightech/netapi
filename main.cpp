372bf91e2e658c2afb4d9c707675a7e1473ece35
aightech <aiiight.tech@gmail.com>
2018-05-14 16:10:07 +0200

    TCP added

--- main.cpp
+++ main.cpp
@@ -7,35 +7,35 @@
 
 int main()
 {
-       NetAPI api;
-       api.verbose();
-       api.setConnectable();
-       api.setConnectionPhrase((char*)"ok");
-       
-       api.startReceiver(2000);
-       
-       
-       char enter[50];
-       //cin>>enter;
-       api.connectToServer(2000,(char *)"127.0.0.1");
-       //waitSec(2);
-       cin>>enter;
-       
-       api.getClientAddr();
-       
-       api.sentToClient(0,(char*)"Mhey");
-       api.sentToClient(0,(char*)"Mhoyy");
-       api.sentToClient(0,(char*)"Mhoazda");
-       cin>>enter;
-       
-       int n;
-       cout << "start" << endl;
-       while((n=api.getReceiverBuffer(enter))>-1)
-       {
-              cout << "buffer n°"<< n<< ": "<<enter<< endl;
-       }
-       cin>>enter;
-       api.endReceiver();
-       //
-       return 0;
+	NetAPI api;
+	api.verbose();
+	api.setConnectable();
+	api.setConnectionPhrase((char*)"ok");
+
+	api.startReceiver(2000,(char*)"TCP");
+
+
+	char enter[50];
+	//cin>>enter;
+	api.connectToServer(2000,(char *)"127.0.0.1");
+	//waitSec(2);
+	cin>>enter;
+
+	api.getClientAddr();
+
+	api.sentToClientTCP(0,(char*)"Mhey");
+	api.sentToClientTCP(0,(char*)"Mhoyy");
+	api.sentToClientTCP(0,(char*)"Mhoazda");
+	cin>>enter;
+
+	int n;
+	cout << "start" << endl;
+	while((n=api.getReceiverBuffer(enter))>-1)
+	{
+		cout << "buffer n°"<< n<< ": "<<enter<< endl;
+	}
+	cin>>enter;
+	api.endReceiver();
+	//
+	return 0;
 }
